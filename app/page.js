f4376e44abfdf639e35865ea2c0ca7dac4117acb
manisha-13-code <manishachauhan5469@gmail.com>
2025-01-17 11:48:01 +0530

    commit

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
+import HeroSection from "@/components/hero";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <Button variant="destructive">Subscribe</Button>
+    <div className="mt-40">
+      <HeroSection/>
+    </div>
   );
 }
 
