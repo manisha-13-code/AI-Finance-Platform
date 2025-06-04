72acded824cababc5d88ac2872e8d2e33d9e5d4d
manisha-13-code <manishachauhan5469@gmail.com>
2025-06-04 23:56:09 +0530

    commit

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { howItWorksData, statsData } from "@/Data/landing";
 import { featuresData } from "@/Data/landing";
 import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -46,13 +47,36 @@ export default function Home() {
           <h2 className="text-3xl font-bold text-center mb-16">How It Works</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {howItWorksData.map((step, index) => (
-              <div key={index}>
-                <div className="text-center p-6 bg-white rounded-lg shadow-md">
+              <div key={index} className="text-center">
+                <div className="text-center p-6 bg-blue-100 rounded-full flex items-center justify-center max-auto mb-6">
                   {step.icon}
                 </div>
                 <h3 className="text-xl font-semibold mt-4">{step.title}</h3>
                 <p className="text-gray-600 mt-2">{step.description}</p>
               </div>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section className="py-20">
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold text-center mb-12">
+            What our Users say
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            {featuresData.map((testimonial, index) => (
+              <Card key={index} className="p-6">
+                <CardContent className="pt-4">
+                  <div>
+                    <Image
+                      src={testimonial.image}
+                      alt={testimonial.name}
+                      className="w-16 h-16 rounded-full mx-auto mb-4"
+                    />
+                  </div>
+                </CardContent>
+              </Card>
             ))}
           </div>
         </div>
