b53ae37d78a358ee0cbca351862bf8525b73e870
manisha-13-code <manishachauhan5469@gmail.com>
2025-07-08 23:06:15 +0530

    commit

--- app/transaction/create/page.jsx
+++ app/transaction/create/page.jsx
@@ -1,28 +1,32 @@
+export const dynamic = "force-dynamic";
+
 import { getAccounts } from "@/actions/dashboard";
 import { defaultCategories } from "@/Data/categories"
 import AddTransactionForm from "../_components/transaction-format";
 import { getTransaction } from "@/actions/transaction";
 
-
 export default async function AddTransactionPage({ searchParams }) {
   const accounts = await getAccounts();
 
-   const editId = searchParams?.edit;
+  const editId = searchParams?.edit;
 
   let initialData = null;
   if (editId) {
     const transaction = await getTransaction(editId);
     initialData = transaction;
   }
+
   return (
     <div className="max-w-3xl mx-auto px-5 mt-20">
       <div className="flex justify-center md:justify-normal mb-8">
-        <h1 className="text-5xl gradient-title ">{editId ? "Edit" : "Add"} Transaction</h1>
+        <h1 className="text-5xl gradient-title ">
+          {editId ? "Edit" : "Add"} Transaction
+        </h1>
       </div>
       <AddTransactionForm
         accounts={accounts}
         categories={defaultCategories}
-         editMode={!!editId}
+        editMode={!!editId}
         initialData={initialData}
       />
     </div>
